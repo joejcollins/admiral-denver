500f95cf6e7f81c60ac3d98e0fcd9e33795eaa37
Joe J Collins <joejcollins@users.noreply.github.com>
2025-04-07 10:18:13 +0000

    Add a symlink to the data

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -7,3 +7,8 @@ cargo install tex-fmt
 # Create the virtual environment.
 make venv
 .venv/bin/pre-commit run --all-files
+
+# Get the data and symlink to it.
+cd ..
+git clone https://github.com/joejcollins/atlanta-shore.git
+ln -s  ../atlanta-shore/data ./admiral-denver/data
