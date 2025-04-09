b20110efe9dbd696a3be150534eb638c82759865
Joe J Collins <j.collins@zengenti.com>
2025-04-09 09:52:00 +0000

    Give permissions for data directory

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -3,6 +3,7 @@ make venv
 .venv/bin/pre-commit run --all-files
 
 # Get the data and symlink to it.
+sudo chown -R vscode:vscode /workspaces
 cd ..
 git clone --depth=1 https://github.com/joejcollins/atlanta-shore.git
 ln -s  ../atlanta-shore/data ./admiral-denver/data
